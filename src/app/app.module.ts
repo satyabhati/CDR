5b7e9cb88cfde8113e370b9a2f33bbcf9397374f
Bhati <52024807@acsind.com>
2021-04-13 22:24:34 +0530

    CDR formio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormioModule, FormioAppConfig } from 'angular-formio';
+import { AppConfig } from '../config';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormioModule
   ],
-  providers: [],
+  providers: [{provide: FormioAppConfig, useValue: AppConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+
+export const AppConfig = {
+    appUrl: 'https://dhnhwgjxyybjuic.form.io',
+    apiUrl: 'https://api.form.io'
+  };
