e67b1d68d8786dda9e0d976a00d57704b6d54d0c
Bhati <52024807@acsind.com>
2021-04-16 10:56:48 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormioModule, FormioAppConfig } from 'angular-formio';
 import { AppConfig } from '../config';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { ClaimsComponent } from './components/claims/claims.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ClaimsComponent
   ],
   imports: [
     BrowserModule,
-    FormioModule
+    FormioModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ClaimsComponent
+      },
+      {
+        path: 'claims',
+        component: ClaimsComponent
+      },
+    
+    ], {useHash: true})
+  
   ],
   providers: [{provide: FormioAppConfig, useValue: AppConfig}],
   bootstrap: [AppComponent]
